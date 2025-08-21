ef50d3c21e18e171781cd70dec2d54b97f45d270
htdocs <guiguiholtz@gmail.com>
2025-08-21 19:09:23 -0300

    novo

---
+++ livraria/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'livraria'  # 👈 para usar livraria:livro_lista etc.
+
+urlpatterns = [
+    path('', views.livro_lista, name='livro_lista'),
+    path('livros/novo/', views.livro_criar, name='livro_criar'),
+    path('livros/<int:pk>/', views.livro_detalhe, name='livro_detalhe'),
+    path('livros/<int:pk>/editar/', views.livro_editar, name='livro_editar'),
+    path('livros/<int:pk>/excluir/', views.livro_excluir, name='livro_excluir'),
+    path('teste/', views.livro_teste, name='livro_teste'),
+]
--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Livro
+from .forms import LivroForm
+from django.http import HttpResponse
 
-# Create your views here.
+def livro_lista(request):
+    livros = Livro.objects.all().order_by('titulo')
+    return render(request,
+                  'livraria/livro_list.html',
+                  {'livros': livros})
+
+def livro_criar(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('livraria:livro_lista')
+    else:
+        form = LivroForm()
+    return render(request,
+                  'livraria/livro_form.html',
+                  {'form': form, 'titulo_pagina': 'Adicionar Livro'})
+
+def livro_detalhe(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    return render(request,
+                  'livraria/livro_detail.html',
+                  {'livro': livro})
+
+def livro_editar(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('livraria:livro_detalhe', pk=livro.pk)
+    else:
+        form = LivroForm(instance=livro)
+
+    return render(request,
+                  'livraria/livro_form.html',
+                  {'form': form, 'livro': livro, 'titulo_pagina': 'Editar Livro'})
+
+def livro_excluir(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        livro.delete()
+        return redirect('livraria:livro_lista')
+
+    return render(request,
+                  'livraria/livro_confirm_delete.html',
+                  {'livro': livro})
+
+def livro_teste(request):
+    return HttpResponse('<h1>Teste da Livraria</h1>')
