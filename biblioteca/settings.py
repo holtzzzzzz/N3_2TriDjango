e9a2673dbb3dc5ec5f76ff27f9c0f53f51158765
htdocs <guiguiholtz@gmail.com>
2025-08-21 19:42:52 -0300

    novo

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
