8d778baac7f4197434cdeb357058c70679ca9a87
htdocs <guiguiholtz@gmail.com>
2025-08-21 19:05:11 -0300

    novo

--- livraria/models.py
+++ livraria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    ano_publicacao = models.PositiveIntegerField()
+    sinopse = models.TextField()
+
+
+    def __str__(self):
+      return f"{self.titulo} ({self.autor})"
